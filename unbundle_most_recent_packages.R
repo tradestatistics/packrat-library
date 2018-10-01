2946fedd743798f3474db95e17f807c75083e0d6
Mauricio Vargas <mvargas@dcc.uchile.cl>
2018-10-01 12:24:36 -0300

    removing tars, adding src instead

--- unbundle_most_recent_packages.R
+++
@@ -1,7 +0,0 @@
-if (!require("packrat")) install.packages("packrat")
-library(packrat)
-
-bundles <- list.files(pattern = "tar")
-
-last_bundle <- bundles[[length(bundles)]]
-unbundle(last_bundle, getwd())
