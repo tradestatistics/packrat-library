a20aa97fed5bc8a1761272dd517c37c189a90b69
Mauricio Vargas <mvargas@dcc.uchile.cl>
2018-11-23 16:01:38 -0300

    updated all packages after moving to R 3.5.1

--- 0-create-bundle.R
+++
@@ -1,2 +0,0 @@
-packrat::snapshot()
-packrat::bundle(file = paste0("packages-snapshot-", Sys.Date(), ".tar.gz"))
---
+++ 1-bundle.R
@@ -0,0 +1,2 @@
+packrat::snapshot()
+packrat::bundle(file = paste0("packages-snapshot-", Sys.Date(), ".tar.gz"))
