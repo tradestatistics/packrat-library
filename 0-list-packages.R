75a4e6630024473c39bef950b3ceeea424efc05b
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-03-14 04:24:08 +0000

    minimal packages for server

--- 0-list-packages.R
+++ 0-list-packages.R
@@ -1,6 +1,6 @@
 # Open ts-packrat-library.Rproj before running this script
 
-# Copyright (c) 2018, Mauricio \"Pacha\" Vargas
+# Copyright (c) 2018-2020, Mauricio \"Pacha\" Vargas
 # This file is part of Open Trade Statistics project
 # The scripts within this project are released under GNU General Public License 3.0
 # See https://github.com/tradestatistics/ts-packrat-library/LICENSE for the details
@@ -8,50 +8,22 @@
 # Packrat does not catch all the packages loaded via pacman so here's the full list
 # across all repositories of this project
 
-#install.packages(c("packrat, "pacman", "data.table", "jsonlite", "readxl", "RPostgreSQL", 
-#                   "tabulizer", "dplyr", "tidyr", "stringr", "janitor", "purrr", 
-#                   "rlang", "Matrix", "doParallel", "glue", "plumber"))
-
-# package management ------------------------------------------------------
-
-library(pacman)
-
-# read/write files --------------------------------------------------------
-
-library(data.table)
-library(jsonlite)
-library(readr)
-library(readxl)
-
-# read/write DB -----------------------------------------------------------
-
-library(RPostgreSQL) 
-
-# extract data from PDFs --------------------------------------------------
-
-library(tabulizer)
-
-# work with data ----------------------------------------------------------
-
-library(dplyr)
-library(tidyr)
-library(stringr)
-library(janitor)
-library(purrr)
-library(rlang)
-library(Matrix)
-
-# API ---------------------------------------------------------------------
+# API ----
 
 library(plumber)
+library(dplyr)
+library(RPostgreSQL)
+library(pool)
+library(stringr)
 library(glue)
+library(readr)
 
-# Plots -------------------------------------------------------------------
+# Visualization ----
 
-library(ggplot2)
-
-# parallelization (Unix only) ---------------------------------------------
-
-if (Sys.info()["sysname"] != "Windows") {
-  library(doParallel)
-}
+library(shiny)
+library(shinyjs)
+library(tidyr)
+library(highcharter)
+library(tradestatistics)
+library(rio)
+library(statquotes)
